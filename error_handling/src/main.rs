5b0c15a160e6ccece7b09eee2d4a9c81cfdf2771
Daniel Miller <hello@dancancodeit.com>
2024-08-26 22:46:21 +0200

    some error handling when file handling goes ape shit

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    panic!("test panic")
+    let greeting_file_result = match File::open("hello.txt") {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(err) => panic!("failed creating file {err}"),
+            },
+            other_error => panic!("not sure wtf is going on here but i can't handle it: {other_error:?}"),
+        },
+    };
 }
