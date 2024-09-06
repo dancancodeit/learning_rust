e3bf7d7a1d0a9c69d870667e59038167bc92cb33
Daniel Miller <hello@dancancodeit.com>
2024-09-06 19:21:59 +0200

    traits practice

---
+++ traits/target/debug/deps/traits-cc0d0c7352435881.d
@@ -0,0 +1,5 @@
+/Users/danielmiller/Workspace/learning_rust/traits/target/debug/deps/libtraits-cc0d0c7352435881.rmeta: src/main.rs
+
+/Users/danielmiller/Workspace/learning_rust/traits/target/debug/deps/traits-cc0d0c7352435881.d: src/main.rs
+
+src/main.rs:
