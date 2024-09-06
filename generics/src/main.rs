bd507d0acfa40ce20e06fc2863f28097c58677af
Daniel Miller <hello@dancancodeit.com>
2024-09-06 18:31:21 +0200

    first generic

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,21 @@
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![1, 23, 32, 232];
+
+    let result = largest(&number_list);
+
+    let char_list = vec!['a', 's', 'f', 'a'];
+    
+    let result = largest(&char_list);
 }
